f7dd020e2b02b002936ce331684ed9263c829fa6
Jordhy-jpg <177513224+Jordhy-jpg@users.noreply.github.com>
2026-03-29 11:53:25 +0700

    hard reset

--- Backend/API/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    path("", views.CreateNoteListView.as_view(), name="note-list"),
-    path("delete/<int:pk>/", views.DeleteNoteView.as_view(), name="note-delete"),
-]
