1124fd615bbb94f245bee52ad56f9c498a5646c5
Jordhy-jpg <177513224+Jordhy-jpg@users.noreply.github.com>
2026-03-29 09:39:20 +0700

    t3

--- Backend/Config/settings.py
+++ Backend/Config/settings.py
@@ -87,7 +87,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
 CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"https:\/\/[^/]+\.vercel\.app",
+    r"^https:\/\/[^/]+\.vercel\.app$",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
