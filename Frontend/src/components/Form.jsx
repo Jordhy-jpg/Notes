f95472e6daac4ab3f8e7398570a8b32f271a52aa
Jordhy-jpg <177513224+Jordhy-jpg@users.noreply.github.com>
2026-03-29 20:37:57 +0700

    completed register/login functionality

---
+++ Frontend/src/components/Form.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../API";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+function Form({ route, method }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, { username, password })
+      if (method === "login") {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const name = method === "login" ? "Login" : "Register";
+
+  return <form onSubmit={handleSubmit}>
+    <h1>{name}</h1>
+
+    <br />
+    <label htmlFor="username">Username</label>
+    <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+
+    <br />
+    <label htmlFor="password">Password</label>
+    <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+
+    <br />
+    <button type="submit">{name}</button>
+  </form>
+}
+
+export default Form
--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,6 +1,8 @@
 function NotFound() {
-    return <div>
-        404 Not Found
+  return (
+    <div>
+      <h1>404 Not Found</h1>
     </div>
+  );
 }
-export default NotFound
+export default NotFound;
